306a527d093bd7aa4480d6ab96f16d6eaa1989d2
اseyede kimia mousavi kalager <seyedekimiamousavikalager@gmail.com>
2026-08-02 20:45:56 +0330

    feat: add ProductList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import ProductList from "./components/ProductList";
 
 function App() {
   return (
     <>
       <Header />
+      <ProductList />
     </>
   );
 }
---
+++ src/components/ProductList.jsx
@@ -0,0 +1,9 @@
+function ProductList() {
+  return (
+    <section>
+      <h2>محصولات برنج</h2>
+    </section>
+  );
+}
+
+export default ProductList;
