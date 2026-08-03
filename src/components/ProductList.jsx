3e4f7bfd19022725ec926317065b68c06cfc91de
اseyede kimia mousavi kalager <seyedekimiamousavikalager@gmail.com>
2026-08-03 22:06:42 +0330

    feat: add useEffect to ProductList

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,4 +1,8 @@
+import { useEffect } from "react";
 function ProductList() {
+  useEffect(() => {
+    console.log("ProductList mounted");
+  }, []);
   return (
     <section>
       <h2>محصولات برنج</h2>
