0b890d0ec586391169059460546bd4d603a470d2
اseyede kimia mousavi kalager <seyedekimiamousavikalager@gmail.com>
2026-08-04 17:29:49 +0330

    feat: add count state to ProductList

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,11 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
 function ProductList() {
+  const [count, setCount] = useState(0);
   useEffect(() => {
     console.log("ProductList mounted");
   }, []);
   return (
     <section>
       <h2>محصولات برنج</h2>
+      <p>تعداد: {count}</p>
+
+      <button onClick={() => setCount(count + 1)}>اضافه کردن</button>
     </section>
   );
 }
