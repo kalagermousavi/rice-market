117ba04c4a26b4dcade5abc93b60575884815338
اseyede kimia mousavi kalager <seyedekimiamousavikalager@gmail.com>
2026-08-12 17:09:38 +0330

    feat: practice useState and useEffect

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from "react";
+
 function Cart() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("Count updated:", count);
+  }, [count]);
+
   return (
     <section>
       <h2>سبد خرید</h2>
+      <p>تعداد محصول: {count}</p>
+      <button onClick={() => setCount(count + 1)}>افزایش تعداد</button>
     </section>
   );
 }
